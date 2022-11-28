10b431dcbe8f6b2c1f25ef93236eed4d746c6bc8
sani-bagh <saba012@student.kristiania.no>
2022-11-28 22:07:30 +0100

    changed data

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,15 +1,11 @@
+
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.40.0"
     }
-  }
-
-  backend "s3" {
-    bucket = "pgr301-1006-terraform"
-    key    = "pgr301-1006-terraform.state"
-    region = "eu-west-1"
   }
 
 }
